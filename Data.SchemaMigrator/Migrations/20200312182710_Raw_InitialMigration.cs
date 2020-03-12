2c4425e5460939749c64ebd1b15696279eb3a913
ChristopheHvd <havard.christophe@gmail.com>
2020-03-12 19:29:10 +0100

    feat(migrations): applied initial migrations

---
+++ Data.SchemaMigrator/Migrations/20200312182710_Raw_InitialMigration.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Data.SchemaMigrator.Migrations
+{
+    public partial class Raw_InitialMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {}
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+           
+        }
+    }
+}
---
+++ Data.SchemaMigrator/Migrations/Bi/20200312182800_Bi_InitialMigration.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Data.SchemaMigrator.Migrations.Bi
+{
+    public partial class Bi_InitialMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+          
+        }
+    }
+}
