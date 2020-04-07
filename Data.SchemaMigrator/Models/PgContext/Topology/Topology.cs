ec6d17342de6d6c6f71da718c17e285b28477160
ChristopheHvd <havard.christophe@gmail.com>
2020-04-07 12:12:12 +0200

    add topology schema

---
+++ Data.SchemaMigrator/Models/PgContext/Topology/Topology.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Data.SchemaMigrator.Models.PgContext.Topology
+{
+    public partial class Topology
+    {
+        public Topology()
+        {
+            Layer = new HashSet<Layer>();
+        }
+
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Srid { get; set; }
+        public double Precision { get; set; }
+        public bool Hasz { get; set; }
+
+        public virtual ICollection<Layer> Layer { get; set; }
+    }
+}
